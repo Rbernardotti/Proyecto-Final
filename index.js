a25c0dd1fffb2d2362e7bd54bc93359c075a3bc8
Romi Bernardotti <romibernardotti@gmail.com>
2022-12-15 16:29:14 -0300

    Subiendo mi Proyecto Final

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let edad  
+
+edad = prompt("Sos mayor de edad?");
+
+if (edad >= si==Si) 
+
+{
+alert("Puedes entrar, eres mayor de edad.");
+} 
+else {
+    alert ("No sos mayor de edad, entras bajo tu responsabilidad.");
+}
+
